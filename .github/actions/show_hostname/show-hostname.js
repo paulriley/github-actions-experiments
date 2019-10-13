dd363970be0154516f0ee8a5e41dd11f3ddacc2c
Paul Riley <paul@wsnhsolutions.com>
2019-10-13 12:53:27 +0000

    Use os.hostname instead of trying to find an environment variable

--- .github/actions/show_hostname/show-hostname.js
+++ .github/actions/show_hostname/show-hostname.js
@@ -1,7 +1,8 @@
 const core = require('@actions/core');
+const os = require('os');
 
 try {
-  console.log(`The event payload: ${process.env}`);
+  console.log(`The event payload: ${os.hostname}`);
 } catch (error) {
   core.setFailed(error.message);
 }
