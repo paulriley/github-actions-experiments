b12de9d16917ced4c85e6275133c9cceada790d0
Paul Riley <paul@wsnhsolutions.com>
2019-10-13 00:29:00 +0000

    Print the whole env object

--- .github/actions/show_hostname/show-hostname.js
+++ .github/actions/show_hostname/show-hostname.js
@@ -1,7 +1,7 @@
 const core = require('@actions/core');
 
 try {
-  console.log(`The event payload: ${process.env.HOSTNAME}`);
+  console.log(`The event payload: ${process.env}`);
 } catch (error) {
   core.setFailed(error.message);
 }
